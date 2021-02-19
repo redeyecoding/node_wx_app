50cbcfb00dac598d809775bd5ae051c009981326
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-02-19 18:31:52 -0500

    started coded for accessing twilio api

--- config.js
+++ config.js
@@ -11,7 +11,12 @@ enviornment.staging = {
     'httpsPort': 3001,
     'envName': 'staging',
     'hashingSecret': 'LKSJDFjwEIJF2#@%j2faksJD0F8AJ3@^@#$jrfj@fj@',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio':  {
+        'accountSid' : 'AC7c8632a0f02821e220af4066ba58844c',
+        'authToken' : '10f268f23c00f9b826092930e74797ba',
+        'fromPhone' : '+19852758621'
+    }
 };
 
 
@@ -21,7 +26,12 @@ enviornment.production = {
     'httpsPort': 5001,
     'envName': 'production',
     'hashingSecret': '54@$@G2EIJF2#@%j2faksJD0F8AJ3@^@#$jrfj@fj@processee3',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio':  {
+        'accountSid' : 'AC7c8632a0f02821e220af4066ba58844c',
+        'authToken' : '10f268f23c00f9b826092930e74797ba',
+        'fromPhone' : '+19852758621'
+    }
 };
 
 // Determine which enviorment was passed as a command-line arg
