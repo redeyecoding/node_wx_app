3718afada13d3b91370de690b2063cd2867df201
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-03-19 14:09:23 -0400

    added env and config file

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ enviornment.staging = {
     'hashingSecret': 'LKSJDFjwEIJF2#@%j2faksJD0F8AJ3@^@#$jrfj@fj@',
     'maxChecks': 5,
     'twilio':  {
-        'accountSid' : '',
-        'authToken' : '',
-        'fromPhone' : ''
+        'accountSid' : 'AC7c8632a0f02821e220af4066ba58844c',
+        'authToken' : 'fb9b64e5f91ecb08db6e44b0d2c8f279',
+        'fromPhone' : '+19852758621'
     }
 };
 
@@ -28,9 +28,9 @@ enviornment.production = {
     'hashingSecret': '54@$@G2EIJF2#@%j2faksJD0F8AJ3@^@#$jrfj@fj@processee3',
     'maxChecks': 5,
     'twilio':  {
-        'accountSid' : '',
-        'authToken' : '',
-        'fromPhone' : ''
+        'accountSid' : 'AC7c8632a0f02821e220af4066ba58844c',
+        'authToken' : 'fb9b64e5f91ecb08db6e44b0d2c8f279',
+        'fromPhone' : '+19852758621'
     }
 };
 
